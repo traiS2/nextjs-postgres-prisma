b0dddd9270afeaf610318fac1c9a0a4e8f89a244
traiS2 <nguyenhoangtrais2@gmail.com>
2024-05-28 15:40:00 +0700

    test seek.ts file

---
+++ app/script/seed.ts
@@ -0,0 +1,30 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const user1 = await prisma.user.create({
+    data: {
+      name: 'Alice',
+      email: 'alice@example.com',
+    },
+  });
+
+  const user2 = await prisma.user.create({
+    data: {
+      name: 'Bob',
+      email: 'bob@example.com',
+    },
+  });
+
+  console.log({ user1, user2 });
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
